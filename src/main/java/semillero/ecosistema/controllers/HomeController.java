463040af8aa46f033549fb8d518bbf9e3ebdf253
EliasGiova <eliasgiooovanella@gmail.com>
2023-11-07 14:19:54 -0300

    agregado de seguridad, ya sea inicio con gooogle o github(prueba)

---
+++ src/main/java/semillero/ecosistema/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package semillero.ecosistema.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello, Home";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "hello, Secured!";
+    }
+}
