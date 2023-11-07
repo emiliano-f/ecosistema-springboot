e5bdc1ae73c03af1ab5fa418d2a8281cae36ad99
EliasGiova <eliasgiooovanella@gmail.com>
2023-11-07 16:23:50 -0300

    descomentacion de user_repository

--- src/main/java/semillero/ecosistema/repositories/UserRepository.java
+++ src/main/java/semillero/ecosistema/repositories/UserRepository.java
@@ -2,7 +2,7 @@ package semillero.ecosistema.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//}
+import semillero.ecosistema.entities.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
