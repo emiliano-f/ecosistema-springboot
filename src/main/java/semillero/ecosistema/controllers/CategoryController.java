7e7c786b65b0c1005190f168ca01a6dbee44298b
EliasGiova <eliasgiooovanella@gmail.com>
2023-11-14 16:16:35 -0300

    category(entity,repository,service and controller)

---
+++ src/main/java/semillero/ecosistema/controllers/CategoryController.java
@@ -0,0 +1,79 @@
+package semillero.ecosistema.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import semillero.ecosistema.entities.Category;
+import semillero.ecosistema.services.CategoryService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveCategory(@Valid @RequestBody Category category) {
+        try {
+            Category savedCategory = categoryService.save(category);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(savedCategory);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"error\": \"An error occurred while processing the request.\"}");
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCategory(@Valid @RequestBody Category category, @PathVariable Long id) {
+        try {
+            Category updateCategory = categoryService.update(id, category);
+            return ResponseEntity.ok(updateCategory);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"error\": \"An error occurred while processing the request.\"}");
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        try {
+            categoryService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("{\"error\": \"An error occurred while processing the request.\"}");
+        }
+    }
+
+    @GetMapping("/allCategories")
+    public ResponseEntity<?> findAllCategories() {
+        try {
+            List<Category> categories = categoryService.findAllCategories();
+            return ResponseEntity.ok(categories);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("{\"error\": \"An error occurred while processing the request.\"}");
+        }
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<?> findCategoryById(@PathVariable Long id){
+        try {
+            Optional<Category> categoryById = categoryService.findById(id);
+            if (categoryById.isPresent()) {
+                return ResponseEntity.ok(categoryById.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/semillero/ecosistema/entities/Category.java
@@ -0,0 +1,25 @@
+package semillero.ecosistema.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name may not be blank")
+    @NotNull
+    private String name;
+}
