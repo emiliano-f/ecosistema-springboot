770653d655b63ed57f91448bc3cb5f3aa428abb4
EliasGiova <eliasgiooovanella@gmail.com>
2023-11-22 16:59:33 -0300

    adding properties in SecurityConfig

--- src/main/java/semillero/ecosistema/configuration/security/SecurityConfig.java
+++ src/main/java/semillero/ecosistema/configuration/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,9 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/countries/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/provinces/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .cors(cors -> cors
