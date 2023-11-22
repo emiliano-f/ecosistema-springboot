4481649f792c20a2d0860a0528e2264321bc7025
EliasGiova <eliasgiooovanella@gmail.com>
2023-11-22 11:15:19 -0300

    add entity, service, repo, controller of country and province

---
+++ src/main/java/semillero/ecosistema/entities/Province.java
@@ -0,0 +1,29 @@
+package semillero.ecosistema.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Province {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    @NotBlank(message = "Name may not be blank")
+    @NotNull
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+}
